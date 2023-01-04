8234ae53553e85ec2114161db78eedc63d16a42b
Jaydenchao71 <3461926217@qq.com>
2023-01-04 19:13:57 +0800

    update 3.0

--- starter.py
+++ starter.py
@@ -3,6 +3,8 @@ from tkinter import messagebox #load messagebox from tkinter
 import os#load os
 import time#load time
 import threading#load threading
+import configparser #load configparser
+import webbrowser #load webbrowser
 windows = tk.Tk()#windows is tk.Tk()
 windows.title('Minecraft Server')#window title is Minecraft Server
 windows.geometry('600x600')#window geometry is 600x600
@@ -11,30 +13,41 @@ windows.geometry('600x600')#window geometry is 600x600
 #Fuction
 def stop():#Fuction stop
     exit()#exit python
+def version():#Fuction version
+    messagebox.showinfo("version","You are using 3.0")#info text "You are using 3.0"
 #end
+def github():#Fuction github
+    webbrowser.open("https://github.com/JaydenStudio/PY-Minecraft-Server")#open github
 #Start
 def start(): #Fuction start
+    config = configparser.ConfigParser()
+    config.read("config.ini")#read config.ini
     serverfile=os.path.exists("./Minecraft/server.jar")#True or False
     if serverfile==True: #if serverfile is True
         print("Server starts in 5 seconds")#print "Server starts in 5 seconds"
         time.sleep(5)#sleep 5 seconds
         os.system("cd Minecraft && java -jar server.jar")#Start server
-        xx = os.path.isfile("./config.txt")#True or False
+        xx = os.path.isfile("./config.ini")#True or False
         if xx == True:#if xx is True
-            with open("config.txt", "r", encoding="utf-8") as f:#open the config.txt
-                y = f.read()#y is text file
-                if y == "Autorestart = True" or y == "Autorestart=True" or y == "Autorestart= True" or y == "Autorestart =True":#if Autorestart is True
-                    z = True#z is True
+            try:
+                Autorestart = config['config']['Autorestart']#read config
+            except KeyError:
+                with open("./config.ini","w",encoding="utf-8") as f:#write file
+                    f.write("[config]\n")
+                    f.write("Autorestart = True")
+                    start()
+            else:#if config is not empty
+                Autorestart = config['config']['Autorestart']#read the config file
+                if Autorestart == True or Autorestart == "true":#if Autorestart is True
                     start()#restart the server
-                else:#if y is False
-                        z = False#z is False
-                        print("Server closed")#print "Server closed"
-                        exit()#closed python
+                else:#if Autorestart is False
+                    print("Server closed")#print Server closed
+                    exit()#exit
         else:#if xx is False
-            with open("config.txt", "w", encoding="utf-8") as f:#new file "config.txt"
-                    f.write("Autorestart = True")#write "Autorestart = True"
-                    z = True#z is True
-                    start()#restart server
+            with open("./config.ini","w",encoding="utf-8") as f:#open the config file
+                f.write("[config]\n")#write the config file
+                f.write("Autorestart = True")#write the autorestart
+                start()#restart the server
     else:#if serverfile is False
         print("Error: server.jar not found")#print "Error: server.jar not found"
 #end
@@ -55,6 +68,20 @@ btn = tk.Button(text="Start!")#Button text "Start"
 btn.config(bg="skyblue")#set button color "skyblue"
 btn.config(width=10,height=5)#set button width "10" and height "5"
 btn.config(command=main)#set button command "main"
+btn.pack()
+#end
+#Button
+btn = tk.Button(text="version")
+btn.config(bg="skyblue")#set button color "skyblue"
+btn.config(width=10,height=5)#set button width "10" and height "5"
+btn.config(command=version)#set button command "version"
+btn.pack()
+#end
+#Button
+btn = tk.Button(text="Github")
+btn.config(bg="skyblue")#set button bg "skyblue"
+btn.config(width=10,height=5)
+btn.config(command=github)
 btn.pack()
 #end
 messagebox.showinfo("Tips:","Use jdk17 or above")#messagebox info text "Use jdk17 or above"
